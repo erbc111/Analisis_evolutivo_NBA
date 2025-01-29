f0cd66c1e12fa07520fe54e9513a679fc4553991
Eris Blanco <erbc111@gmail.com>
2025-01-29 12:31:06 -0500

    Se modifica el scrip correctamente

--- ejecutar_notebook.py
+++ ejecutar_notebook.py
@@ -10,4 +10,6 @@ with open(notebook_path, 'r', encoding='utf-8') as f:
 
 # Ejecutar el notebook
 ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-ep.preprocess(nb, {'metadata': {'path': 'ruta/al/directorio'}})
+ep.preprocess(nb, {'metadata': {'path': r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA"}})
+
+print("Ejecutado correctamente")
