5058028f7f903ee2a4bd012e1d3282065ee7a024
Eris Blanco <erbc111@gmail.com>
2025-01-29 12:25:02 -0500

    Se crea scrip que leera los notebooks y los ejecutara

---
+++ ejecutar_notebook.py
@@ -0,0 +1,13 @@
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+# Ruta al archivo .ipynb
+notebook_path = r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA\carga_ingesta_SQL.ipynb"
+
+# Cargar el notebook
+with open(notebook_path, 'r', encoding='utf-8') as f:
+    nb = nbformat.read(f, as_version=4)
+
+# Ejecutar el notebook
+ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+ep.preprocess(nb, {'metadata': {'path': 'ruta/al/directorio'}})
