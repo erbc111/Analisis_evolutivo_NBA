b7b0e221e2c59721e2ede08434881ac8baab7a5d
Eris Blanco <erbc111@gmail.com>
2025-01-29 20:41:39 -0500

    Se modifica el scrip correctamente

--- ejecutar_notebook.py
+++ ejecutar_notebook.py
@@ -6,7 +6,9 @@ notebook_path = r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Anali
 
 # Cargar el notebook
 with open(notebook_path, 'r', encoding='utf-8') as f:
-    nb = nbformat.read(f, as_version=4)
+    nb = nbformat.read(f, as_version=4) #nbformat.read: Es una función de la biblioteca nbformat que lee un archivo .ipynb 
+                                        #  y lo convierte en un objeto de Python que representa el notebook. La f es que se abre en solo lectura
+                                        # version 
 
 # Ejecutar el notebook
 # El ExecutePreprocessor es el encargado de ejecutar el notebook.
@@ -16,6 +18,9 @@ with open(notebook_path, 'r', encoding='utf-8') as f:
 #kernel_name: Es el nombre del kernel de Jupyter que se utilizará para ejecutar el notebook.
 
 ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+
+
 ep.preprocess(nb, {'metadata': {'path': r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA"}})
 
 print("Ejecutado correctamente")
