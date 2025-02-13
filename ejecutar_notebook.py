077a171226b730b4fc1756f0b7529ede2ee5f1a7
Eris Blanco <erbc111@gmail.com>
2025-02-12 22:38:08 -0500

    Cambios al tablero

--- ejecutar_notebook.py
+++ ejecutar_notebook.py
@@ -9,7 +9,6 @@ with open(notebook_path, 'r', encoding='utf-8') as f:
     nb = nbformat.read(f, as_version=4) #nbformat.read: Es una función de la biblioteca nbformat que lee un archivo .ipynb 
                                         #  y lo convierte en un objeto de Python que representa el notebook. La f es que se abre en solo lectura
                                         # version 
-
 # Ejecutar el notebook
 # El ExecutePreprocessor es el encargado de ejecutar el notebook.
 # timeout: Es el tiempo máximo (en segundos) que se permitirá que el notebook se ejecute. 
@@ -19,80 +18,30 @@ with open(notebook_path, 'r', encoding='utf-8') as f:
 
 ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
 
-
-
 ep.preprocess(nb, {'metadata': {'path': r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA"}})
-
 print("Descarga ejecutada correctamente")
-
 # Ruta al archivo .ipynb
 notebook_path = r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA\preparacion_datos.ipynb"
-
 # Cargar el notebook
 with open(notebook_path, 'r', encoding='utf-8') as f:
-    nb = nbformat.read(f, as_version=4) #nbformat.read: Es una función de la biblioteca nbformat que lee un archivo .ipynb 
-                                        #  y lo convierte en un objeto de Python que representa el notebook. La f es que se abre en solo lectura
-                                        # version 
-
-# Ejecutar el notebook
-# El ExecutePreprocessor es el encargado de ejecutar el notebook.
-# timeout: Es el tiempo máximo (en segundos) que se permitirá que el notebook se ejecute. 
-# Si el notebook tarda más que este tiempo, se detendrá. Por ejemplo, timeout=600 permite 10 minutos.
-
-#kernel_name: Es el nombre del kernel de Jupyter que se utilizará para ejecutar el notebook.
-
+    nb = nbformat.read(f, as_version=4) 
 ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-
-
-
 ep.preprocess(nb, {'metadata': {'path': r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA"}})
-
 print("Datos preparados correctamente")
-
 # Ruta al archivo .ipynb
 notebook_path = r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA\carga_ingesta_SQL.ipynb"
-
 # Cargar el notebook
 with open(notebook_path, 'r', encoding='utf-8') as f:
-    nb = nbformat.read(f, as_version=4) #nbformat.read: Es una función de la biblioteca nbformat que lee un archivo .ipynb 
-                                        #  y lo convierte en un objeto de Python que representa el notebook. La f es que se abre en solo lectura
-                                        # version 
-
-# Ejecutar el notebook
-# El ExecutePreprocessor es el encargado de ejecutar el notebook.
-# timeout: Es el tiempo máximo (en segundos) que se permitirá que el notebook se ejecute. 
-# Si el notebook tarda más que este tiempo, se detendrá. Por ejemplo, timeout=600 permite 10 minutos.
-
-#kernel_name: Es el nombre del kernel de Jupyter que se utilizará para ejecutar el notebook.
-
+    nb = nbformat.read(f, as_version=4) 
 ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-
-
-
 ep.preprocess(nb, {'metadata': {'path': r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA"}})
-
 print("Datos inyectados correctamente")
-
 # Ruta al archivo .ipynb
 notebook_path = r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA\descarga_temporada_actual.ipynb"
-
 # Cargar el notebook
 with open(notebook_path, 'r', encoding='utf-8') as f:
-    nb = nbformat.read(f, as_version=4) #nbformat.read: Es una función de la biblioteca nbformat que lee un archivo .ipynb 
-                                        #  y lo convierte en un objeto de Python que representa el notebook. La f es que se abre en solo lectura
-                                        # version 
-
-# Ejecutar el notebook
-# El ExecutePreprocessor es el encargado de ejecutar el notebook.
-# timeout: Es el tiempo máximo (en segundos) que se permitirá que el notebook se ejecute. 
-# Si el notebook tarda más que este tiempo, se detendrá. Por ejemplo, timeout=600 permite 10 minutos.
-
-#kernel_name: Es el nombre del kernel de Jupyter que se utilizará para ejecutar el notebook.
-
+    nb = nbformat.read(f, as_version=4) 
 ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-
-
-
 ep.preprocess(nb, {'metadata': {'path': r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA"}})
 
 print("Descarga e inyeccion de temporada actual ejecutada correctamente")
@@ -102,22 +51,8 @@ notebook_path = r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Anali
 
 # Cargar el notebook
 with open(notebook_path, 'r', encoding='utf-8') as f:
-    nb = nbformat.read(f, as_version=4) #nbformat.read: Es una función de la biblioteca nbformat que lee un archivo .ipynb 
-                                        #  y lo convierte en un objeto de Python que representa el notebook. La f es que se abre en solo lectura
-                                        # version 
-
-# Ejecutar el notebook
-# El ExecutePreprocessor es el encargado de ejecutar el notebook.
-# timeout: Es el tiempo máximo (en segundos) que se permitirá que el notebook se ejecute. 
-# Si el notebook tarda más que este tiempo, se detendrá. Por ejemplo, timeout=600 permite 10 minutos.
-
-#kernel_name: Es el nombre del kernel de Jupyter que se utilizará para ejecutar el notebook.
-
+    nb = nbformat.read(f, as_version=4) 
 ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-
-
-
 ep.preprocess(nb, {'metadata': {'path': r"C:\Users\ERBC\Desktop\curso henry\Analisis_evolutivo_NBA\Analisis_evolutivo_NBA"}})
-
 print("Scrapping de estadisticas de todos los jugadores realizado correctamente")
 
